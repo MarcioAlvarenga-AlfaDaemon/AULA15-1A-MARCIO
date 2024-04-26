db9125aa56f6e10ad6dd3ef76e1091d816a6c8af
MarcioAlvarenga-AlfaDaemon <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2024-04-26 12:33:44 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,15 @@
-//Aula 16 Pensamento Computacional
-let cor;
-let posicaoHorizontal;
-let posicaoVertical;
-
+//preparar e rodar um código
 function setup() {
-  createCanvas(400, 400);
-  background("pink");
-  cor = color(random(0, 255), random(0, 255), random(0, 255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
+  createCanvas(800, 800); //responsável para criar uma tela com 400 de largura e 400 de altura
+  background("rgb(124,17,17)");
 }
 
+//responsável pela função de desenhar na tela
 function draw() {
-  fill(cor);
-  circle(posicaoHorizontal, posicaoVertical, 50);
-
-  if (mouseX < posicaoHorizontal) {
-    posicaoHorizontal--;
-  }
-
-  if (mouseX > posicaoHorizontal) {
-    posicaoHorizontal++;
-  }
-
-  if (mouseY < posicaoVertical) {
-    posicaoVertical--;
-  }
-
-  if (mouseY > posicaoVertical) {
-    posicaoVertical++;
-  }
-
-  if (mouselsPressed) {
-    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  stroke("black");
+  fill("white");
+  
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 40, 40);
   }
 }
